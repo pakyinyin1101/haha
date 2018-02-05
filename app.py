d6e28884a218cc50eeb625c5f48cc4852fd65e24
ayueei <yuewang@cse.cuhk.edu.hk>
2018-02-05 20:22:32 +0800

    add index.html and modify app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ server_address = ("", PORT)
 
 httpd = server_class(server_address, handler_class)
 
-url = 'http://localhost:{0}/{1}'.format(PORT, script_path)
+# url = 'http://localhost:{0}/{1}'.format(PORT, script_path)
+url = 'http://localhost:{0}/{1}'.format(PORT, 'index.html')
 
 webbrowser.open_new_tab(url)
 
